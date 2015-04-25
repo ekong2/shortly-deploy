7fe03708f47d076a0486df771a858e9318454ec1
Hack Reactor Students <accounts@hackreactor.com>
2015-04-24 20:02:51 -0700

    first part done, mongo refactor tomorrow

--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,19 @@
-var db = require('../config');
+var mongoose = require('mongoose');
+var shortlyDb = require('/config.js');
 var crypto = require('crypto');
 
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  initialize: function(){
-    this.on('creating', function(model, attrs, options){
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
-});
+var Link = new mongoose.model('Link', shortlyDb.urlSchema);
 
 module.exports = Link;
+  // tableName: 'urls',
+  // hasTimestamps: true,
+  // defaults: {
+  //   visits: 0
+  // },
+  // initialize: function(){
+  //   this.on('creating', function(model, attrs, options){
+  //     var shasum = crypto.createHash('sha1');
+  //     shasum.update(model.get('url'));
+  //     model.set('code', shasum.digest('hex').slice(0, 5));
+  //   });
+  // }
