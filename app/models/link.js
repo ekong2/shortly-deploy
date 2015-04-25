3c7abd9f17704890d22582c620bf4bcfd0c5c556
Eddie Kong <eddiek93@gmail.com>
2015-04-25 11:56:09 -0700

    mongoose refactor, problems with promisify

--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,25 @@
-var mongoose = require('mongoose');
-var shortlyDb = require('/config.js');
-var crypto = require('crypto');
+var mongoose = require('mongoose'),
+    crypto   = require('crypto');
 
-var Link = new mongoose.model('Link', shortlyDb.urlSchema);
+var LinkSchema = new mongoose.Schema({
+ visits: Number,
+ link: String,
+ title: String,
+ code: String,
+ base_url: String,
+ url: String
+});
 
-module.exports = Link;
-  // tableName: 'urls',
-  // hasTimestamps: true,
-  // defaults: {
-  //   visits: 0
-  // },
-  // initialize: function(){
-  //   this.on('creating', function(model, attrs, options){
-  //     var shasum = crypto.createHash('sha1');
-  //     shasum.update(model.get('url'));
-  //     model.set('code', shasum.digest('hex').slice(0, 5));
-  //   });
-  // }
+var createSha = function(url) {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(url);
+  return shasum.digest('hex').slice(0, 5);
+};
+
+LinkSchema.pre('save', function(next){
+  var code = createSha(this.url);
+  this.code = code;
+  next();
+});
+
+module.exports = mongoose.model('Link', LinkSchema);
